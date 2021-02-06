1b9b31e58d208d4830675101c980d8236976f00a
jianxin <615845421@qq.com>
2021-02-06 21:33:21 +0800

    mail

---
+++ src/main/java/com/jianxin/community/util/MailClient.java
@@ -0,0 +1,43 @@
+package com.jianxin.community.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+
+//util包存放工具类
+@Component
+public class MailClient {
+    private static final Logger logger = LoggerFactory.getLogger(MailClient.class);
+
+    @Autowired
+    private JavaMailSender mailSender;
+    //发件人
+    @Value("${spring.mail.username}")
+    private String from;
+
+                                        //主题                内容
+    public void sendMail(String to ,String subject ,String content) throws MessagingException {
+
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message);
+            helper.setFrom(from);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(content,true);
+            mailSender.send(helper.getMimeMessage());
+        } catch (MessagingException e) {
+            logger.error("发送邮件失败" + e.getMessage());
+        }
+
+    }
+}
